7e8130d3886affd6591ad5e0cc9c1466b8fc9efb
迫間 秀虎 <fko2247003@stu.o-hara.ac.jp>
2022-09-28 11:34:00 +0900

    待ち時間を設定 車の速度を設定

--- car.cpp
+++ car.cpp
@@ -2,10 +2,20 @@
 #include "game.h"
 #include "car.h"
 
+namespace
+{
+	//待ち時間
+	constexpr int kWaitFrameMin = 60;
+	constexpr int kWaitFrameMax = 180;
+	//車の速度
+	constexpr float kSpeed = -24.0f;
+}
+
 Car::Car()
 {
 	m_handle = -1;
 	m_fieldY = 0.0f;
+	m_waitFrame = 0;
 }
 
 void Car::setGraphic(int handle)
@@ -22,10 +32,17 @@ void Car::setup(float fieldY)
 
 	m_vec.x = -16.0f;
 	m_vec.y = 0.0f;
+
+	//動き始めるまでの時間を設定	一秒から三秒待つ	60フレームから180フレーム
+	m_waitFrame=GetRand(kWaitFrameMax -kWaitFrameMin)+ kWaitFrameMin;
 }
 
 void Car::update()
 {
+	if (m_waitFrame > 0) {
+		m_waitFrame--;
+		return;
+	}
 	m_pos += m_vec;
 }
 
